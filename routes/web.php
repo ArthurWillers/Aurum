1a01f5ee109560e580799445197e47bf36465f42
willers <arthur2008willers@gmail.com>
2025-07-29 22:11:24 -0300

    Remove email verification functionality and related components

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,9 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
 Route::middleware(['auth'])->group(function () {
+    Route::view('dashboard', 'dashboard')->name('dashboard');
+    
     Route::redirect('settings', 'settings/profile');
 
     Route::get('settings/profile', Profile::class)->name('settings.profile');
