bc2baaccb37ddd49182bf1f311860913785b0572
willers <arthur2008willers@gmail.com>
2025-07-31 19:04:04 -0300

    refactor: remove redundant foreign key migration for expenses table

--- database/migrations/2025_07_31_203536_change_category_foreign_key_on_delete_in_transactions.php
+++ database/migrations/2025_07_31_203536_change_category_foreign_key_on_delete_in_transactions.php
@@ -11,18 +11,6 @@ return new class extends Migration
      */
     public function up(): void
     {
-        // Altera a tabela de despesas (expenses)
-        Schema::table('expenses', function (Blueprint $table) {
-            // Remove a chave estrangeira antiga
-            $table->dropForeign(['category_id']);
-
-            // Adiciona a nova chave estrangeira com restrição de exclusão
-            $table->foreign('category_id')
-                ->references('id')
-                ->on('categories')
-                ->restrictOnDelete();
-        });
-
         // Altera a tabela de receitas (incomes)
         Schema::table('incomes', function (Blueprint $table) {
             // Remove a chave estrangeira antiga
