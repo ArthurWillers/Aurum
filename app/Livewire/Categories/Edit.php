5ec2b5ad850dfcd77383499ea99fa19886cab335
willers <arthur2008willers@gmail.com>
2025-07-31 17:59:52 -0300

    feat: enhance category edit form with input fields and buttons for better user interaction

--- app/Livewire/Categories/Edit.php
+++ app/Livewire/Categories/Edit.php
@@ -2,10 +2,41 @@
 
 namespace App\Livewire\Categories;
 
+
 use Livewire\Component;
+use App\Models\Category;
+use App\Enums\CategoryType;
+use Illuminate\Validation\Rules\Enum;
+use Livewire\Attributes\Validate;
+
 
 class Edit extends Component
 {
+    public Category $category;
+
+    #[Validate('required|string|min:3|max:255')]
+    public string $name = '';
+
+    #[Validate('required', new Enum(CategoryType::class))]
+    public string $type = '';
+
+    public function mount()
+    {
+        $this->name = $this->category->name;
+        $this->type = $this->category->type->value;       
+    }
+
+    public function update()
+    {
+        $this->validate();
+        $this->category->update([
+            'name' => $this->name,
+            'type' => $this->type,
+        ]);
+        session()->flash('success', 'Categoria atualizada com sucesso!');
+        return $this->redirect('/categories', navigate: true);
+    }
+
     public function render()
     {
         return view('livewire.categories.edit');
--- resources/views/livewire/categories/edit.blade.php
+++ resources/views/livewire/categories/edit.blade.php
@@ -1,3 +1,35 @@
 <div>
-    {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
+    {{-- O formulário chama o método "update" do componente ao ser enviado --}}
+    <form wire:submit="update" class="flex flex-col gap-6">
+
+        {{-- Cabeçalho da página com título e botões --}}
+        <div class="flex justify-between items-center">
+            <h2 class="text-2xl font-bold text-zinc-900 dark:text-white">
+                Editar Categoria
+            </h2>
+            <flux:button href="{{ route('categories.index') }}" wire:navigate.persist>Voltar</flux:button>
+        </div>
+
+        <div class="bg-white dark:bg-zinc-800 rounded-lg shadow-xl border border-zinc-200 dark:border-zinc-700 p-6">
+            <div class="flex flex-col gap-6">
+
+                <flux:input wire:model="name" label="Nome da Categoria" id="name"
+                    placeholder="Ex: Salário, Supermercado, Lazer" autofocus />
+
+                <flux:radio.group wire:model="type" label="Tipo" class="py-1">
+                    <flux:radio value="expense" label="Despesa" />
+                    <flux:radio value="income" label="Receita" />
+                </flux:radio.group>
+
+                <div class="flex justify-between">
+                    <flux:button href="{{ route('categories.index') }}" wire:navigate.persist>
+                        Cancelar
+                    </flux:button>
+                    <flux:button type="submit" variant="primary">
+                        Salvar
+                    </flux:button>
+                </div>
+            </div>
+        </div>
+    </form>
 </div>
