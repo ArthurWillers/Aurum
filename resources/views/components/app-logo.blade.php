13a1471dde37a68033d4c93398208a1cd6fb8f63
willers <arthur2008willers@gmail.com>
2025-07-31 10:36:54 -0300

    atualiza estilo do app-logo

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,6 +1,9 @@
-<div class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
-    <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
-</div>
-<div class="ms-1 grid flex-1 text-start text-sm">
-    <span class="mb-0.5 truncate leading-tight font-semibold">Aurum</span>
+<div class="flex items-center">
+    <div
+        class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
+        <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
+    </div>
+    <div class="ms-1 grid flex-1 text-start text-lg">
+        <span class="ms-1 truncate leading-tight font-semibold">Aurum</span>
+    </div>
 </div>
