23cd9fedcb4a8bd593a41e3c7f1639b4492cd222
willers <arthur2008willers@gmail.com>
2025-08-01 14:27:01 -0300

    feat: add Portuguese language files for authentication, pagination, password reset, and validation

---
+++ lang/pt_BR/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+  /*
+    |--------------------------------------------------------------------------
+    | Linhas de Linguagem de Reset de Senha
+    |--------------------------------------------------------------------------
+    |
+    | As seguintes linhas de linguagem são as linhas padrão que correspondem aos motivos
+    | dados pelo broker de senha para uma tentativa de atualização de senha,
+    | como falha devido a uma senha inválida / token de reset.
+    |
+    */
+
+  'reset' => 'Sua senha foi redefinida.',
+  'sent' => 'Enviamos por email o link de redefinição da sua senha.',
+  'throttled' => 'Por favor, aguarde antes de tentar novamente.',
+  'token' => 'Este token de redefinição de senha é inválido.',
+  'user' => "Não conseguimos encontrar um usuário com esse endereço de email.",
+
+];
